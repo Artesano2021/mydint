82452d17957fe3349e7a7820a54fcde31e9a99db
Artesano2021 <95882772+Artesano2021@users.noreply.github.com>
2023-02-24 15:16:33 +0100

    kkk
    nueva

---
+++ tema3/albumAngular/src/app/galeria2/galeria2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria2',
+  templateUrl: './galeria2.component.html',
+  styleUrls: ['./galeria2.component.scss']
+})
+export class Galeria2Component {
+
+}
